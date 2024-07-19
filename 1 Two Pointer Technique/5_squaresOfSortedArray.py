a0a0eeb3ec8a46034b12716a2d83e6e62a4c31d2
SoeRatch <surajkoho@gmail.com>
2024-07-19 16:44:45 +0530

    squares of sorted array

---
+++ 1 Two Pointer Technique/5_squaresOfSortedArray.py
@@ -0,0 +1,35 @@
+# Leetcode link- https://leetcode.com/problems/squares-of-a-sorted-array/
+
+# Squares of a Sorted Array
+
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+# Example 1:
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+
+# Example 2:
+# Input: nums = [-7,-3,2,3,11]
+# Output: [4,9,9,49,121]
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        result=[0 for i in range(n)]
+        low,high=0,n-1
+        curr=n-1
+
+        while low<=high:
+            if abs(nums[low])>=abs(nums[high]):
+                result[curr]=nums[low]**2
+                low+=1
+            else:
+                result[curr]=nums[high]**2
+                high-=1
+            curr-=1
+        
+        return result
+        
